6bcc2d3f7ea5649d3117cc1c642ef60c8ed53caf
Aprixia <48212681+Aprixia@users.noreply.github.com>
2020-07-15 16:02:29 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -43,7 +43,7 @@ module.exports = {
             let embed = new MessageEmbed()
             .setColor("RANDOM")
             .setTitle(command.name.slice(0,1).toUpperCase() + command.name.slice(1))
-            .setDescription(`${command.description}`)
+            .setDescription(`${command.description}\n<> = required | [] = optional`)
             .addField(`Usage`, prefix+command.name+' '+command.usage, true)
             .addField(`Category`, command.category.slice(0,1).toUpperCase()+command.category.slice(1), true)
             .addField(`Aliases`, `${command.aliases.length > 0 ? command.aliases.join(', ') : "None" }`)
