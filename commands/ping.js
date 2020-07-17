8c40fd00f797bc8f84331910ffc72489e59e3295
Aprixia <48212681+Aprixia@users.noreply.github.com>
2020-07-18 00:17:42 +0200

    Create ping.js

---
+++ commands/ping.js
@@ -0,0 +1,13 @@
+module.exports = {
+    name: "ping",
+    description: "Returns API and latency",
+    
+    async code(client, message, args, isTest) {
+        const msg = await message.channel.send("Pinging... ")
+        await msg.edit(`
+        🏓 Pong! 🏓
+        Ping is \`${client.ws.ping}ms\`
+        Latency is \`${msg.createdTimestamp - message.createdTimestamp}ms\`
+        `)
+    }
+}
