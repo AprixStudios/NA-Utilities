3d8b6a13b07d8483b4572eb0e5b778b7894e593e
Aprixia <48212681+Aprixia@users.noreply.github.com>
2020-07-18 13:39:12 +0200

    Update slowmode.js

--- commands/slowmode.js
+++ commands/slowmode.js
@@ -13,10 +13,11 @@ module.exports = {
     async code(client, message, args, isTest) {
         if (!message.member.hasPermission("MANAGE_MESSAGES") && isTest === false) return message.channel.send(`No. Missing Permissions: ${message.member.hasPermission("MANAGE_MESSAGES") ? "" : "Manage Messages"}`);
         var delay = parseInt(args[0]);
-        if (!delay) {
+        if (!args[0]) {
             let embed = new MessageEmbed().setColor("RANDOM").setTitle(`Syntax Error\n${this.name.slice(0,1).toUpperCase()+this.name.slice(1)}`).setDescription(`${this.description}\n\nDelay is not defined.`).addField(`Usage`, prefix+this.name+' '+this.usage)
             return message.channel.send(embed);
         }
+        args.shift();
         if (isNaN(delay)) delay = 0;
         if (args.length !== 0 && ['-c','-clean'].includes(args[args.length-1].toLowerCase())) args.pop();
         var reason = args.slice(0).join(' ');
