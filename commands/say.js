8f815ec3b19e5ba6b6a45b3fc4586a12ea698e78
Aprixia <48212681+Aprixia@users.noreply.github.com>
2020-07-18 13:46:00 +0200

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -15,6 +15,16 @@ module.exports = {
             let embed = new MessageEmbed().setColor("RANDOM").setTitle(`Syntax Error\n${this.name.slice(0,1).toUpperCase()+this.name.slice(1)}`).setDescription(`${this.description}`).addField(`Usage`, prefix+this.name+' '+this.usage);
             return message.channel.send(embed);
         }
-        message.channel.send(`${args.join(' ')}`);
+        message.channel.send(`${args.join(' ')}`).then(msg => {
+            setTimeout(() => {
+                if (message.deleted) return msg.delete();
+                setTimeout(() => {
+                    if (message.deleted) return msg.delete();
+                    setTimeout(() => {
+                        if (message.deleted) return msg.delete();
+                    }, 5000);
+                }, 5000);
+            }, 5000);
+        });
     }
 }
